e591014cb67f0efc2863db704a63d4d6123ae01c
Vehikl Team <go@vehikl.com>
2019-05-07 10:18:58 -0400

    Create App spec

---
+++ components/__tests__/App.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Task from '../Task';
+import { TouchableHighlight } from 'react-native';
+import { isTemplateElement } from '@babel/types';
+
+describe('App', () => {
+  it('does something', () => {
+    expect(true).toBe(true);
+  })
+})
