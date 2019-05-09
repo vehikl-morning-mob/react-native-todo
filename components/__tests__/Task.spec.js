36c2416dc602196a95a8fbdb31207a67335f3d49
Vehikl Team <go@vehikl.com>
2019-05-09 10:37:10 -0400

    Remove clutter

--- components/__tests__/Task.spec.js
+++ components/__tests__/Task.spec.js
@@ -2,7 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Task from '../Task';
 import { TouchableHighlight } from 'react-native';
-import { isTemplateElement } from '@babel/types';
 
 describe('Incoming Queries to a Task', () => {
   it('displays the task', () => {
