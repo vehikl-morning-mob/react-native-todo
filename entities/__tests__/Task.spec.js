8832e741bf138128ce84aec4bdae0d3269af0704
Vehikl Team <go@vehikl.com>
2019-05-13 15:52:01 -0400

    cleanup imports

--- entities/__tests__/Task.spec.js
+++ entities/__tests__/Task.spec.js
@@ -1,16 +1,16 @@
-import TaskEntity from '../../entities/task';
+import Task from '../task';
 
 
 describe('Incoming Commands on Task', () => {
   it('can be marked as complete', () => {
-    const task = new TaskEntity();
+    const task = new Task();
     expect(task.completed).toBe(false);
     task.markComplete();
     expect(task.completed).toBe(true);
   })
 
   it('accepts name of task', () => {
-    const task = new TaskEntity(1, "foo");
+    const task = new Task(1, "foo");
     expect(task.name).toBe("foo");
   })
 })
