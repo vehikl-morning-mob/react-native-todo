22b9caba41b0d8a30b7020644855146f52860677
Vehikl Team <go@vehikl.com>
2019-05-21 14:30:55 -0400

    Refactor toggle task complete

--- entities/__tests__/Task.spec.js
+++ entities/__tests__/Task.spec.js
@@ -3,17 +3,11 @@ import Task from '../task';
 
 describe('Incoming Commands on Task', () => {
 
-  it('can be marked as complete', () => {
+  it('can toggle complete', () => {
     const task = new Task();
     expect(task.completed).toBe(false);
-    task.markComplete();
-    expect(task.completed).toBe(true);
-  })
-
-  it('can be marked as incomplete', () => {
-    const task = new Task();
     task.toggleComplete();
-    expect(task.completed).toBe(true)
+    expect(task.completed).toBe(true);
   })
 
   it('accepts name of task', () => {
