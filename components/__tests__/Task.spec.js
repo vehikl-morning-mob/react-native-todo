edb1cfe796899a8f5cfd5df50c4dfc34422b0c01
Vehikl Team <go@vehikl.com>
2019-05-21 14:08:52 -0400

    nest suites in a suite for task

--- components/__tests__/Task.spec.js
+++ components/__tests__/Task.spec.js
@@ -4,41 +4,42 @@ import Task from '../Task';
 import { TouchableHighlight } from 'react-native';
 import TaskEntity from '../../entities/task';
 
+describe('Task', () => {
+  describe('Incoming Queries to a Task', () => {
+    it('displays the task', () => {
+      const task = new TaskEntity(1, 'Foo')
 
-describe('Incoming Queries to a Task', () => {
-  it('displays the task', () => {
-    const task = new TaskEntity(1, 'Foo')
+      const component = renderer.create(<Task task={task} />).toJSON();
+      expect(component).toMatchSnapshot();
+    })
 
-    const component = renderer.create(<Task task={task} />).toJSON();
-    expect(component).toMatchSnapshot();
+    it('displays completed task', () => {
+      const task = {
+        id: 1,
+        name: 'Foo',
+        completed: true,
+      }
+      const component = renderer.create(<Task task={task} />).toJSON();
+      expect(component).toMatchSnapshot();
+    })
   })
 
-  it('displays completed task', () => {
-    const task = {
-      id: 1,
-      name: 'Foo',
-      completed: true,
-    }
-    const component = renderer.create(<Task task={task} />).toJSON();
-    expect(component).toMatchSnapshot();
-  })
-})
+  describe('Outgoing Commands from Task Component', () => {
+    it('toggles completed task on click', () => {
+      const task = {
+        id: 1,
+        name: 'Foo',
+        completed: false,
+      }
+      const toggleCompleted = jest.fn(() => {
+        task.completed = true;
+      });
+      const component = renderer.create(<Task task={task} toggleCompleted={toggleCompleted} />).root;
 
-describe('Outgoing Commands from Task Component', () => {
-  it('toggles completed task on click', () => {
-    const task = {
-      id: 1,
-      name: 'Foo',
-      completed: false,
-    }
-    const toggleCompleted = jest.fn(() => {
-      task.completed = true;
-    });
-    const component = renderer.create(<Task task={task} toggleCompleted={toggleCompleted} />).root;
+      component.findByType(TouchableHighlight).props.onPress();
 
-    component.findByType(TouchableHighlight).props.onPress();
-
-    expect(toggleCompleted).toHaveBeenCalledWith(task);
-    expect(task.completed).toBe(true);
+      expect(toggleCompleted).toHaveBeenCalledWith(task);
+      expect(task.completed).toBe(true);
+    })
   })
 })
