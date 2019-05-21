53a120552af1c8fab3d655de8e7bb2acb72983c7
Vehikl Team <go@vehikl.com>
2019-05-21 14:07:05 -0400

    remove duplicated tests

--- components/__tests__/Task.spec.js
+++ components/__tests__/Task.spec.js
@@ -42,17 +42,3 @@ describe('Outgoing Commands from Task Component', () => {
     expect(task.completed).toBe(true);
   })
 })
-
-describe('Incoming Commands on Task', () => {
-  it('can be marked as complete', () => {
-    const task = new TaskEntity();
-    expect(task.completed).toBe(false);
-    task.markComplete();
-    expect(task.completed).toBe(true);
-  })
-
-  it('accepts name of task', () => {
-    const task = new TaskEntity(1, "foo");
-    expect(task.name).toBe("foo");
-  })
-})
